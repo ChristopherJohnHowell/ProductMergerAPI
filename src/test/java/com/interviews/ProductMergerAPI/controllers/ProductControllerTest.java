a1fe269ee0c600dbbb5c6942d2805ea090255f5d
Chris Howell <monostation1@gmail.com>
2024-06-25 22:07:06 +0100

    Adding ProductControllerTest for Integration testing

--- src/test/java/com/interviews/ProductMergerAPI/controllers/ProductControllerTest.java
+++ src/test/java/com/interviews/ProductMergerAPI/controllers/ProductControllerTest.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
@@ -20,9 +22,21 @@ public class ProductControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    public void getProductsByType_withValidData_ShouldReturnData() {
-        // CJHTEST
+    public void getProductsByType_withValidData_ShouldReturnHttp200() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/products?productType=BASIC"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
+    @Test
+    public void getProductsByType_withInvalidData_ShouldReturnHttp204() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/products?productType=xxxxxxxx"))
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    @Test
+    public void getProductsByType_withNoData_ShouldReturnHttp200() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/products"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
 
 }
